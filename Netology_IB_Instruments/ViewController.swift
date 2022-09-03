78a9ad3bfcfc636d0caa8008714bf7794cb8c2a5
Mikhail Shornikov <michail.shornikov@gmail.com>
2022-09-03 11:09:30 +0300

    Initial Commit

---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Шорников Михаил on 03.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
